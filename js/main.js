665d1307762fb16b7ad545ec647e6d89e7116d13
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-06-05 17:50:26 -0300

    Integração com a API do IBGE para obter as cidades e municípios do Brasil.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
-// https://servicodados.ibge.gov.br/api/v1/localidades/estados
-// https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
+// Execução
+getStates()
+
+// Monitora o select "states" e quando existe uma mudança(change), executa a função getCities.
+document.querySelector("select[name=states]").addEventListener("change", getCities)
+
+// Função para obter a lista de estados do brasil através da API do IBGE[JSON]
+function getStates() {
+    const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    const statesSelect = document.querySelector("select[name=states]");
+
+    fetch(url)
+        .then(res => res.json())
+        .then(states => {
+            for (state of states) {
+                statesSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+            }
+        });
+}
+
+// Função para obter a lista de municípios de cada estado através da API do IBGE[JSON]
+function getCities(event) {
+    const uf = event.target.value
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`
+    const citiesSelect = document.querySelector("select[name=cities]")
+    const inputState = document.querySelector("input[name=state]");
+    const inputCity = document.querySelector("input[name=city]");
+
+    inputState.value = event.target.options[event.target.selectedIndex].label
+    inputCity.value = event.target.options[event.target.selectedIndex].label
+
+    fetch(url)
+        .then(res => res.json())
+        .then(citiesList => {
+            for (city of citiesList) {
+                citiesSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+            }
+
+            citiesSelect.disabled = false
+        });
+}
