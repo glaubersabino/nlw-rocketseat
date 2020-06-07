89e4b6cc5edd2b8b0b6ec699b9427c71cdb32ab1
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-06-07 01:18:40 -0300

    Instalação do servidor nodejs, refatoração dos arquivos e pastas para organização do projeto e configurações iniciais de um projeto com o express.

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const server = express()
+
+server.use(express.static("public"))
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create", (req, res) => {
+    res.sendFile(__dirname + "/views/create.html")
+})
+
+server.get("/search", (req, res) => {
+    res.sendFile(__dirname + "/views/search.html")
+})
+
+
+server.listen(3000)
