c374a6b740439f0aa5dd3664e7f4c803e57739bb
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-06-11 10:55:33 -0300

    Adição do SQLite ao projeto e inicialização do CRUD.

---
+++ src/database/db.js
@@ -0,0 +1,39 @@
+const sqlite3 = require("sqlite3").verbose()
+
+const db = new sqlite3.Database("./src/database/database.db")
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    db.run(query, [
+        "https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1161&q=80",
+        "Colectoria",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ])
+})
