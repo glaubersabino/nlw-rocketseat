9e04540899c80e8c752858d1278deadfd0f316b8
Glauber Albuquerque <glauber.sabino19@gmail.com>
2020-06-12 10:15:59 -0300

    Exportação do objeto do banco de dados para que ele possa ser usado  no restanta da aplicação.

--- src/database/db.js
+++ src/database/db.js
@@ -2,73 +2,75 @@ const sqlite3 = require("sqlite3").verbose()
 
 const db = new sqlite3.Database("./src/database/database.db")
 
-db.serialize(() => {
+module.exports = db
 
-    // Cria uma tabela no SQLite
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-        );
-    `)
+// db.serialize(() => {
 
-    // Define constantes para adicionar dados em uma tabela.
-    const query = `
-        INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-        ) VALUES (?,?,?,?,?,?,?);
-    `
+//     // Cria uma tabela no SQLite
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//         );
+//     `)
 
-    const values = [
-        "https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1161&q=80",
-        "Colectoria",
-        "Guilherme Gemballa, Jardim América",
-        "260",
-        "Santa Catarina",
-        "Rio do Sul",
-        "Resíduos Eletrônicos, Lâmpadas"
-    ]
+//     // Define constantes para adicionar dados em uma tabela.
+//     const query = `
+//         INSERT INTO places (
+//             image,
+//             name,
+//             address,
+//             address2,
+//             state,
+//             city,
+//             items
+//         ) VALUES (?,?,?,?,?,?,?);
+//     `
 
-    // Insere dados numa tabela
-    db.run(query, values, afterInsertData)
+//     const values = [
+//         "https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1161&q=80",
+//         "Colectoria",
+//         "Guilherme Gemballa, Jardim América",
+//         "260",
+//         "Santa Catarina",
+//         "Rio do Sul",
+//         "Resíduos Eletrônicos, Lâmpadas"
+//     ]
 
-    // Função para vericar se houve erro na inserção.
-    function afterInsertData(err) {
-        if (err) {
-            return console.log(err)
-        }
+//     // Insere dados numa tabela
+//     db.run(query, values, afterInsertData)
 
-        console.log("Cadastrado com sucesso!")
-        console.log(this)
-    }
+//     // Função para vericar se houve erro na inserção.
+//     function afterInsertData(err) {
+//         if (err) {
+//             return console.log(err)
+//         }
 
-    // Deleta dados de uma tabela.
-    db.run(`DELETE FROM places WHERE id = ?`, [2], function (err) {
-        if (err) {
-            return console.log(err)
-        }
+//         console.log("Cadastrado com sucesso!")
+//         console.log(this)
+//     }
 
-        console.log("Deletado com sucesso!")
-    })
+//     // Deleta dados de uma tabela.
+//     db.run(`DELETE FROM places WHERE id = ?`, [2], function (err) {
+//         if (err) {
+//             return console.log(err)
+//         }
 
-    // Consulta dados de uma tabela.
-    db.all(`SELECT * FROM places`, function (err, rows) {
-        if (err) {
-            return console.log(err)
-        }
+//         console.log("Deletado com sucesso!")
+//     })
 
-        console.log(rows)
-    })
-})
+//     // Consulta dados de uma tabela.
+//     db.all(`SELECT * FROM places`, function (err, rows) {
+//         if (err) {
+//             return console.log(err)
+//         }
+
+//         console.log(rows)
+//     })
+// })
